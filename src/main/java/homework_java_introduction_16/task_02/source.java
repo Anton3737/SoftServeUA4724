427ec1d3a760037f4399e90d22523b2cb64bd44e
Anton <BarashASUA@gmail.com>
2025-08-17 14:35:36 +0300

    Home work 16 has done.

---
+++ src/main/java/homework_java_introduction_16/task_02/source.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public String myAttribute1 = "value";
+    public int myAttribute2;
+
+    public void publicMethod1() {
+        System.out.println("Це публічний метод.");
+    }
+
+    public String getMyAttribute1() {
+        return myAttribute1;
+    }
+
+    void anotherMethod() {
+        String text = "public_string";
+        System.out.println(text);
+    }
+}
